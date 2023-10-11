69565fe098ad2655847c9010ea925b29871b56e3
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-10-11 13:50:39 +0200

    Created HelpersTests

---
+++ test/foundry/HelpersTests.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.21;
+
+
+import {IERC20Permit} from "../../contracts/interfaces/IERC20Permit.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "solady/src/utils/FixedPointMathLib.sol";
+
+
+library HelpersTests {
+
+    using FixedPointMathLib for uint;
+
+    function getPermitHash(
+        address token_,
+        address owner_,
+        address spender_,
+        uint value_,
+        uint nonce_,
+        uint deadline_
+    ) internal view returns(bytes32) {
+        return keccak256(
+                    abi.encodePacked(
+                        "\x19\x01",
+                        IERC20Permit(token_).DOMAIN_SEPARATOR(),
+                        keccak256(
+                            abi.encode(
+                                keccak256(
+                                    "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+                                ),
+                                owner_,
+                                spender_,
+                                value_,
+                                nonce_,
+                                deadline_
+                            )
+                        )
+                    )
+                );
+    }
+
+
+    function calculateMinAmountsOut(
+        address[2] memory feeds_, 
+        uint amountIn_, 
+        uint decimals_,
+        uint slippage_
+    ) internal view returns(uint[] memory) {
+        uint[] memory minAmountsOut = new uint[](2);
+        uint BASE = 18;
+
+        for (uint i=0; i < feeds_.length; i++) {
+            uint decimals = decimals_ == BASE ? BASE : (BASE - decimals_) + decimals_;
+            
+            (,int price,,,) = AggregatorV3Interface(feeds_[i]).latestRoundData();
+            uint expectedOut = 
+                ( i == 0 ? amountIn_ * 10 ** (decimals) : minAmountsOut[i - 1] )
+                .fullMulDiv(1 ether, i == 0 ? uint(price) * 1e10 : uint(price));
+
+            uint minOut = expectedOut - expectedOut.fullMulDiv(slippage_, 10000);
+            minAmountsOut[i] = minOut;
+        }
+
+        return minAmountsOut;
+    }
+
+
+    function calculateMinAmountsOut(
+        uint amount_,
+        uint slippage_
+    ) internal pure returns(uint) {
+        return amount_ - amount_.fullMulDiv(slippage_, 10000);
+    }
+
+
+
+}
--- test/foundry/ozTokenFactory.t.sol
+++ test/foundry/ozTokenFactory.t.sol
@@ -5,14 +5,15 @@ pragma solidity 0.8.21;
 import "../../contracts/facets/ozTokenFactory.sol";
 import {Setup} from "./Setup.sol";
 import "../../contracts/interfaces/ozIToken.sol";
-import "solady/src/utils/FixedPointMathLib.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// import "solady/src/utils/FixedPointMathLib.sol";
+// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {IQueries, IPool, IAsset, IVault} from "../../contracts/interfaces/IBalancer.sol";
 import "../../contracts/libraries/Helpers.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 // import "../../lib/forge-std/src/interfaces/IERC20.sol";
 // import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 import {TradeAmounts} from "../../contracts/AppStorage.sol";
+import {HelpersTests} from "./HelpersTests.sol";
 
 import "forge-std/console.sol";
 
@@ -29,6 +30,10 @@ contract ozTokenFactoryTest is Setup {
     using Helpers for uint[2];
     using Helpers for IVault.JoinKind;
     using Helpers for address[];
+    //-----
+    using HelpersTests for address;
+    using HelpersTests for address[2];
+    using HelpersTests for uint;
    
 
     function test_createOzToken() public {
@@ -40,8 +45,8 @@ contract ozTokenFactoryTest is Setup {
         uint rawAmount = 1000;
         uint amountIn = rawAmount * 10 ** ozUSDC.decimals();
 
-        uint[] memory minsOut = _calculateMinAmountsOut(
-            [ethUsdChainlink, rEthEthChainlink], rawAmount, ozUSDC.decimals()
+        uint[] memory minsOut = [ethUsdChainlink, rEthEthChainlink].calculateMinAmountsOut(
+            rawAmount, ozUSDC.decimals(), defaultSlippage
         );
         
         uint minWethOut = minsOut[0];
@@ -52,19 +57,6 @@ contract ozTokenFactoryTest is Setup {
         address[] memory assets = [wethAddr, rEthWethPoolBalancer, rEthAddr].convertToDynamic();
         uint[] memory maxAmountsIn = [0, 0, minRethOut].convertToDynamic();
         uint[] memory amountsIn = [0, minRethOut].convertToDynamic();
-
-        // bytes memory userData = abi.encode( 
-        //     IVault.JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT,
-        //     amountsIn,
-        //     0
-        // );
-
-        // IVault.JoinPoolRequest memory request = IVault.JoinPoolRequest({
-        //     assets: assets,
-        //     maxAmountsIn: maxAmountsIn,
-        //     userData: userData,
-        //     fromInternalBalance: false
-        // });
 
         IVault.JoinPoolRequest memory request = assets.createRequest(
             maxAmountsIn, IVault.JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT.createUserData(amountsIn, 0)
@@ -77,14 +69,13 @@ contract ozTokenFactoryTest is Setup {
             request
         );
 
-        uint minBptOut = _calculateMinAmountsOut(bptOut);
+        uint minBptOut = bptOut.calculateMinAmountsOut(defaultSlippage);
 
         //---------
 
         vm.startPrank(owner);
 
-        bytes32 permitHash = _getPermitHash(
-            usdcAddr,
+        bytes32 permitHash = usdcAddr.getPermitHash(
             owner,
             address(ozDiamond),
             amountIn,
@@ -109,63 +100,63 @@ contract ozTokenFactoryTest is Setup {
      * Helpers 
      */
    
-    function _getPermitHash(
-        address token_,
-        address owner_,
-        address spender_,
-        uint value_,
-        uint nonce_,
-        uint deadline_
-    ) private view returns(bytes32) {
-        return keccak256(
-                    abi.encodePacked(
-                        "\x19\x01",
-                        IERC20Permit(token_).DOMAIN_SEPARATOR(),
-                        keccak256(
-                            abi.encode(
-                                keccak256(
-                                    "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-                                ),
-                                owner_,
-                                spender_,
-                                value_,
-                                nonce_,
-                                deadline_
-                            )
-                        )
-                    )
-                );
-    }
+    // function _getPermitHash(
+    //     address token_,
+    //     address owner_,
+    //     address spender_,
+    //     uint value_,
+    //     uint nonce_,
+    //     uint deadline_
+    // ) private view returns(bytes32) {
+    //     return keccak256(
+    //                 abi.encodePacked(
+    //                     "\x19\x01",
+    //                     IERC20Permit(token_).DOMAIN_SEPARATOR(),
+    //                     keccak256(
+    //                         abi.encode(
+    //                             keccak256(
+    //                                 "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+    //                             ),
+    //                             owner_,
+    //                             spender_,
+    //                             value_,
+    //                             nonce_,
+    //                             deadline_
+    //                         )
+    //                     )
+    //                 )
+    //             );
+    // }
 
 
-    function _calculateMinAmountsOut(
-        address[2] memory feeds_, 
-        uint amountIn_, 
-        uint decimals_
-    ) private view returns(uint[] memory) {
-        uint[] memory minAmountsOut = new uint[](2);
+    // function _calculateMinAmountsOut(
+    //     address[2] memory feeds_, 
+    //     uint amountIn_, 
+    //     uint decimals_
+    // ) private view returns(uint[] memory) {
+    //     uint[] memory minAmountsOut = new uint[](2);
 
-        for (uint i=0; i < feeds_.length; i++) {
-            uint decimals = decimals_ == _BASE ? _BASE : (_BASE - decimals_) + decimals_;
+    //     for (uint i=0; i < feeds_.length; i++) {
+    //         uint decimals = decimals_ == _BASE ? _BASE : (_BASE - decimals_) + decimals_;
             
-            (,int price,,,) = AggregatorV3Interface(feeds_[i]).latestRoundData();
-            uint expectedOut = 
-                ( i == 0 ? amountIn_ * 10 ** (decimals) : minAmountsOut[i - 1] )
-                .fullMulDiv(1 ether, i == 0 ? uint(price) * 1e10 : uint(price));
+    //         (,int price,,,) = AggregatorV3Interface(feeds_[i]).latestRoundData();
+    //         uint expectedOut = 
+    //             ( i == 0 ? amountIn_ * 10 ** (decimals) : minAmountsOut[i - 1] )
+    //             .fullMulDiv(1 ether, i == 0 ? uint(price) * 1e10 : uint(price));
 
-            uint minOut = expectedOut - expectedOut.fullMulDiv(defaultSlippage, 10000);
-            minAmountsOut[i] = minOut;
-        }
+    //         uint minOut = expectedOut - expectedOut.fullMulDiv(defaultSlippage, 10000);
+    //         minAmountsOut[i] = minOut;
+    //     }
 
-        return minAmountsOut;
-    }
+    //     return minAmountsOut;
+    // }
 
 
-    function _calculateMinAmountsOut(
-        uint256 amount_
-    ) private view returns(uint256 minAmountOut) {
-        minAmountOut = amount_ - amount_.fullMulDiv(defaultSlippage, 10000);
-    }
+    // function _calculateMinAmountsOut(
+    //     uint256 amount_
+    // ) private view returns(uint256 minAmountOut) {
+    //     minAmountOut = amount_ - amount_.fullMulDiv(defaultSlippage, 10000);
+    // }
 
 
 
