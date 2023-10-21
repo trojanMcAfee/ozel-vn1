02bfcbf884b5db64c3c487e891693a420bbb34ea
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-10-21 16:31:14 +0200

    Refactored deploying script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,42 +15,49 @@ const {
   rEthEthChainlink
 } = require("./state-vars");
 
-const { getSelectors, FacetCutAction } = require('./libraries/diamond.js')
+const { getSelectors, FacetCutAction } = require('./libraries/diamond.js');
+
+const { deployContract } = require('./helpers');
 
 async function deployDiamond () {
   const accounts = await ethers.getSigners()
   const contractOwner = accounts[0]
 
   // deploy DiamondCutFacet
-  const DiamondCutFacet = await ethers.getContractFactory('DiamondCutFacet')
-  const diamondCutFacet = await DiamondCutFacet.deploy()
-  await diamondCutFacet.deployed()
-  console.log('DiamondCutFacet deployed:', diamondCutFacet.address)
+  // const DiamondCutFacet = await ethers.getContractFactory('DiamondCutFacet')
+  // const diamondCutFacet = await DiamondCutFacet.deploy()
+  // await diamondCutFacet.deployed()
+  // console.log('DiamondCutFacet deployed:', diamondCutFacet.address)
+  const diamondCutFacet = await deployContract('DiamondCutFacet');
 
   // deploy ozToken as implementation + the Beacon
-  const OzToken = await ethers.getContractFactory('ozToken')
-  const ozToken = await OzToken.deploy()
-  await ozToken.deployed()
-  console.log('ozToken deployed:', ozToken.address)
+  // const OzToken = await ethers.getContractFactory('ozToken')
+  // const ozToken = await OzToken.deploy()
+  // await ozToken.deployed()
+  // console.log('ozToken deployed:', ozToken.address)
+  const ozToken = await deployContract('ozToken');
 
-  const Beacon = await ethers.getContractFactory('ozBeacon')
-  const beacon = await Beacon.deploy(ozToken.address)
-  await beacon.deployed()
-  console.log('ozBeacon deployed:', beacon.address)
+  // const Beacon = await ethers.getContractFactory('ozBeacon')
+  // const beacon = await Beacon.deploy(ozToken.address)
+  // await beacon.deployed()
+  // console.log('ozBeacon deployed:', beacon.address)
+  const beacon = await deployContract('ozBeacon', ozToken.address);
 
   // deploy Diamond
-  const Diamond = await ethers.getContractFactory('Diamond')
-  const diamond = await Diamond.deploy(contractOwner.address, diamondCutFacet.address)
-  await diamond.deployed()
-  console.log('Diamond deployed:', diamond.address)
+  // const Diamond = await ethers.getContractFactory('Diamond')
+  // const diamond = await Diamond.deploy(contractOwner.address, diamondCutFacet.address)
+  // await diamond.deployed()
+  // console.log('Diamond deployed:', diamond.address)
+  const diamond = await deployContract('Diamond', contractOwner.address, diamondCutFacet.address);
 
   // deploy DiamondInit
   // DiamondInit provides a function that is called when the diamond is upgraded to initialize state variables
   // Read about how the diamondCut function works here: https://eips.ethereum.org/EIPS/eip-2535#addingreplacingremoving-functions
-  const DiamondInit = await ethers.getContractFactory('DiamondInit')
-  const diamondInit = await DiamondInit.deploy()
-  await diamondInit.deployed()
-  console.log('DiamondInit deployed:', diamondInit.address)
+  // const DiamondInit = await ethers.getContractFactory('DiamondInit')
+  // const diamondInit = await DiamondInit.deploy()
+  // await diamondInit.deployed()
+  // console.log('DiamondInit deployed:', diamondInit.address)
+  const diamondInit = await deployContract('DiamondInit');
 
   // deploy facets
   console.log('')
--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,19 +1,35 @@
-const { ethers } = require('ethers');
-const { parseEther } = ethers.utils;
+// const { ethers } = require('ethers');
+// const { parseEther } = ethers.utils;
 
 async function sendETHOps(amount, receiver) {
     const [signer] = await hre.ethers.getSigners();
     
     tx = await signer.sendTransaction({
-        value: parseEther(amount.toString()),
+        value: ethers.utils.parseEther(amount.toString()),
         to: receiver,
         gasLimit: ethers.BigNumber.from('5000000'),
         gasPrice: ethers.BigNumber.from('5134698068')
     });
     await tx.wait();
+}
 
+async function deployContract(contractName, arg = false, arg2) {
+    const Contract = await ethers.getContractFactory(contractName);
+    let contract;
+
+    if (contractName == 'Diamond') {
+        contract = await Contract.deploy(arg, arg2);
+    } else {
+        contract = !arg ? await Contract.deploy() : await Contract.deploy(arg)
+    }
+
+    await contract.deployed();
+    console.log(`${contractName} deployed:`, contract.address)
+
+    return contract;
 }
 
 module.exports = {
-    sendETHOps
+    sendETHOps,
+    deployContract
 };
