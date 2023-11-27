e50ff5b2a5be04242199d9a8319f6881330845b5
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-11-27 16:48:56 +0100

    Dai permit experiment works

--- contracts/interfaces/IERC20Permit.sol
+++ contracts/interfaces/IERC20Permit.sol
@@ -42,6 +42,7 @@ interface IERC20Permit {
     function decimals() external view returns (uint8);
 
     function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external view returns (bytes32);
 
     function permit(
         address owner,
@@ -52,6 +53,9 @@ interface IERC20Permit {
         bytes32 r,
         bytes32 s
     ) external;
+
+    function permit(address holder, address spender, uint256 nonce, uint256 expiry,
+                    bool allowed, uint8 v, bytes32 r, bytes32 s) external;
 
     function nonces(address owner) external view returns (uint256);
 }
