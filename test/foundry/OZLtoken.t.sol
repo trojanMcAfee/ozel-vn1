9dfdf37d07ad26a4b2608fa21f93c5e031ac6a84
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-01-16 23:36:11 +0100

    Added test_exchange_rate_equilibrium()

--- test/foundry/OZLtoken.t.sol
+++ test/foundry/OZLtoken.t.sol
@@ -86,82 +86,6 @@ contract OZLtokenTest is TestMethods {
     }
 
 
-    // function test_x() public {
-    //     //Pre-conditions
-    //     ozIToken ozERC20 = ozIToken(OZ.createOzToken(
-    //         testToken, "Ozel-ERC20", "ozERC20"
-    //     ));
-
-    //     (uint rawAmount,,) = _dealUnderlying(Quantity.BIG);
-    //     uint amountIn = rawAmount * 10 ** IERC20Permit(testToken).decimals();
-    //     _changeSlippage(uint16(9900));
-
-    //     _startCampaign();
-    //     _mintOzTokens(ozERC20, alice, amountIn); 
-
-    //     uint secs = 10 + campaignDuration; 
-    //     vm.warp(block.timestamp + secs);
-
-    //     int durationLeft = OZ.durationLeft();
-    //     assertTrue(durationLeft < 0);
-
-    //     _mock_rETH_ETH();
-
-    //     uint earned = OZ.earned(alice);
-    //     assertTrue(earned > 0);
-
-    //     IOZL OZL = IOZL(address(ozlProxy));
-    //     (uint ozlBalanceAlice,) = _checkChargeFeeClaimOZL(OZL);
-
-    //     //Actions
-    //     uint pendingAllocPreRedeem = OZ.pendingAllocation();
-    //     assertTrue(pendingAllocPreRedeem < (1 * 1e18) / 1000000);
-
-    //     vm.startPrank(alice);
-    //     OZL.approve(address(OZL), ozlBalanceAlice);
-    
-    //     OZL.redeem(
-    //         alice,
-    //         alice,
-    //         wethAddr,
-    //         ozlBalanceAlice,
-    //         _getMinsOut(OZL, ozlBalanceAlice, QuoteAsset.ETH)
-    //     );
-    //     vm.stopPrank();
-
-    //     earned = OZ.earned(alice);
-    //     assertTrue(earned == 0);
-
-    //     uint pendingAllocPostRedeem = OZ.pendingAllocation();
-    //     assertTrue(pendingAllocPreRedeem == pendingAllocPostRedeem);
-
-    //     uint ozlBalanceOZPostRedeem = OZL.balanceOf(address(OZ));
-    //     assertTrue(ozlBalanceOZPostRedeem == communityAmount);
-
-    //     uint oldRecicledSupply = OZ.getRecicledSupply();
-    //     assertTrue(oldRecicledSupply == ozlBalanceAlice);
-
-    //     // uint oldRewardRate = OZ.getRewardRate();
-
-    //     //-----
-
-    //     uint newOzlBalanceAlice = OZL.balanceOf(alice);
-    //     assertTrue(newOzlBalanceAlice == 0);
-    
-
-    //     earned = OZ.earned(alice);
-    //     console.log('earned alice post: ', earned);
-
-
-    //     //Actions
-    //     // uint oneYear = 31560000;
-    //     // vm.prank(owner);
-    //     // OZ.startNewReciclingCampaign(oneYear); 
-
-    //     //Post-conditions
-       
-    // }
-
 
     //Tests that a new recicling campaign is properly set up with the recicled supply.
     function test_new_recicling_campaing() public {
@@ -344,6 +268,49 @@ contract OZLtokenTest is TestMethods {
         uint pendingOZLallocPost = OZ.pendingAllocation();
         assertTrue(claimedReward ==  pendingOZLallocPre - pendingOZLallocPost);
     }
+
+
+    function test_exchange_rate_equilibrium() public {
+        //Pre-conditions
+        test_claim_OZL();
+
+        IOZL OZL = IOZL(address(ozlProxy));
+
+        uint ozlBalanceAlice = OZL.balanceOf(alice) / 2;
+        uint rEthBalancePre = IERC20Permit(rEthAddr).balanceOf(alice);
+        assertTrue(rEthBalancePre == 0);
+        
+        uint rEthToRedeem = (ozlBalanceAlice * OZL.getExchangeRate(QuoteAsset.rETH)) / 1 ether;
+        _changeSlippage(uint16(5)); //0.05%
+        uint minAmountOutReth = HelpersLib.calculateMinAmountOut(rEthToRedeem, OZ.getDefaultSlippage());
+
+        uint[] memory minAmountsOut = new uint[](1);
+        minAmountsOut[0] = minAmountOutReth;
+
+        //Action
+        vm.startPrank(alice);
+        approve(OZL, ozlBalanceAlice);
+
+        uint ratePreRedeem = OZL.getExchangeRate();
+
+        uint amountOut = OZL.redeem(
+            alice,
+            alice,
+            rEthAddr,
+            ozlBalanceAlice,
+            minAmountsOut
+        );
+        vm.stopPrank();
+
+        uint ratePostRedeem = OZL.getExchangeRate();
+        assertTrue(ratePreRedeem / 1e5 == ratePostRedeem / 1e5);
+
+        //Post-condition
+        uint rEthBalancePost = IERC20Permit(rEthAddr).balanceOf(alice);
+        
+        assertTrue(rEthBalancePost > 0);
+        assertTrue(amountOut == rEthBalancePost);
+    } 
 
 
 
