b5548993ee09e5768899e257491d9fb715c2d304
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-01-25 00:22:25 +0100

    Added comments to test

--- test/foundry/MultipleTokens.t.sol
+++ test/foundry/MultipleTokens.t.sol
@@ -17,6 +17,7 @@ contract MultipleTokensTest is TestMethods {
 
     
     function test_multiple_ozToken_balances() public {
+        //Pre-conditions
         ozIToken ozERC20_1 = ozIToken(OZ.createOzToken(
             testToken, "Ozel-ERC20-1", "ozERC20_1"
         ));
@@ -32,11 +33,13 @@ contract MultipleTokensTest is TestMethods {
 
         _startCampaign();
 
+        //Actions
         _mintOzTokens(ozERC20_1, alice, testToken, amountInFirst);
         _mintOzTokens(ozERC20_2, alice, secondTestToken, amountInSecond);
         _mintOzTokens(ozERC20_2, bob, secondTestToken, amountInSecond);
         _mintOzTokens(ozERC20_1, charlie, testToken, amountInFirst / 3);
 
+        //Post-condtions
         uint balAlice_oz1 = ozERC20_1.balanceOf(alice);
         uint balAlice_oz2 = ozERC20_2.balanceOf(alice);
         uint balBob_oz2 = ozERC20_2.balanceOf(bob);
@@ -49,6 +52,11 @@ contract MultipleTokensTest is TestMethods {
         //Difference between equal stablecoin balances is less than 0.01%
         diff = balAlice_oz1 - (balCharlie_oz1 * 3);
         assertTrue(diff.mulDivDown(10_000, balAlice_oz1) < 1);
+    }
+
+
+    function test_multiple_OZL_claim() public {
+
     }
 
 
