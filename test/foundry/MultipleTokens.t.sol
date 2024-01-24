5ee61b262d5e46e2a7df1fb74b372c23947a39f8
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-01-25 00:18:11 +0100

    First multiple test ready

--- test/foundry/MultipleTokens.t.sol
+++ test/foundry/MultipleTokens.t.sol
@@ -6,11 +6,14 @@ import {IUniswapV3Factory} from "@uniswap/v3-core/contracts/interfaces/IUniswapV
 import {ozIToken} from "../../contracts/interfaces/ozIToken.sol";
 import {IERC20Permit} from "../../contracts/interfaces/IERC20Permit.sol";
 import {TestMethods} from "./TestMethods.sol";
+import {FixedPointMathLib} from "../../contracts/libraries/FixedPointMathLib.sol";
 
 import "forge-std/console.sol";
 
 
 contract MultipleTokensTest is TestMethods {
+
+    using FixedPointMathLib for uint;
 
     
     function test_x() public {
@@ -42,31 +45,29 @@ contract MultipleTokensTest is TestMethods {
         _startCampaign();
 
         _mintOzTokens(ozERC20_1, alice, testToken, amountInFirst);
-
-        _resetPoolBalances(oldSlot0data, oldSharedCash, cashSlot);
-
         _mintOzTokens(ozERC20_2, alice, secondTestToken, amountInSecond);
-
-        _resetPoolBalances(oldSlot0data, oldSharedCash, cashSlot);
-        
         _mintOzTokens(ozERC20_2, bob, secondTestToken, amountInSecond);
-
-        _resetPoolBalances(oldSlot0data, oldSharedCash, cashSlot);
-
         _mintOzTokens(ozERC20_1, charlie, testToken, amountInFirst / 3);
 
-        uint bal1 = ozERC20_1.balanceOf(alice);
-        uint bal2 = ozERC20_2.balanceOf(alice);
-        uint bal3 = ozERC20_2.balanceOf(bob);
-        
-        // console.log('--- start of bal4 ---');
-        uint bal4 = ozERC20_1.balanceOf(charlie);
-        // console.log('--- end of bal4 ---');
+        uint balAlice_oz1 = ozERC20_1.balanceOf(alice);
+        uint balAlice_oz2 = ozERC20_2.balanceOf(alice);
+        uint balBob_oz2 = ozERC20_2.balanceOf(bob);
+        uint balCharlie_oz1 = ozERC20_1.balanceOf(charlie);
 
-        console.log('bal1 - dai ****: ', bal1); 
-        console.log('bal2 - usdc ****: ', bal2);
-        console.log('bal3 - usdc ****: ', bal3);
-        console.log('bal4 - dai ****: ', bal4);
+        //Difference between non-equal stablecoin balances is less than 0.02%
+        uint diff = (balAlice_oz2 + balBob_oz2) - balAlice_oz1;
+        // assertTrue((diff * 10_000) < 2);
+        assertTrue(diff.mulDivDown(10_000, balAlice_oz1) < 2);
+
+        //Difference between equal stablecoin balances is less than 0.01%
+        diff = balAlice_oz1 - (balCharlie_oz1 * 3);
+        // assertTrue(diff < 1);
+        assertTrue(diff.mulDivDown(10_000, balAlice_oz1) < 1);
+
+        // console.log('bal1 - dai ****: ', bal1); 
+        // console.log('bal2 - usdc ****: ', bal2);
+        // console.log('bal3 - usdc ****: ', bal3);
+        // console.log('bal4 - dai ****: ', bal4);
 
     }
 
