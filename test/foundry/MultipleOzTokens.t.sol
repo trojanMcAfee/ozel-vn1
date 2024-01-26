e308087bca5df2187e6066e2435eac00f27e878a
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-01-26 20:27:39 +0100

    Done with test_two_ozTokens_twoUsers_different_mint()

--- test/foundry/MultipleOzTokens.t.sol
+++ test/foundry/MultipleOzTokens.t.sol
@@ -14,7 +14,9 @@ contract MultipleOzTokensTest is TestMethods {
 
     //Tests the creation of different ozTokens and that their minting of tokens is 
     //done properly. 
-    function test_createAndMint_two_ozTokens_oneUser() public returns(ozIToken, ozIToken, uint) {
+    function test_createAndMint_two_ozTokens_oneUser() public returns(
+        ozIToken, ozIToken, uint, uint
+    ) {
         //Pre-conditions  
         ozIToken ozERC20_1 = ozIToken(OZ.createOzToken(
             testToken, "Ozel-ERC20-1", "ozERC20_1"
@@ -43,7 +45,7 @@ contract MultipleOzTokensTest is TestMethods {
         assertTrue(ozBalance_1 < amountInFirst && ozBalance_1 > (amountInFirst - 1 * 1e18));
         assertTrue(ozBalance_2 < amountInSecond_18dec && ozBalance_2 > (amountInSecond_18dec - 1 * 1e18));
 
-        return (ozERC20_1, ozERC20_2, amountInFirst);
+        return (ozERC20_1, ozERC20_2, amountInFirst, amountInSecond);
     }
 
     
@@ -70,7 +72,7 @@ contract MultipleOzTokensTest is TestMethods {
     //Tests that two users can claim OZL rewards from minting from the same ozToken
     function test_two_ozTokens_twoUsers_same_mint() public {
         //Pre-conditions
-        (ozIToken ozERC20_1, ozIToken ozERC20_2, uint amountInFirst) =
+        (ozIToken ozERC20_1, ozIToken ozERC20_2, uint amountInFirst,) =
              test_createAndMint_two_ozTokens_oneUser();
 
         _mintOzTokens(ozERC20_1, bob, testToken, amountInFirst);
@@ -102,9 +104,38 @@ contract MultipleOzTokensTest is TestMethods {
     }
 
 
-    function test_two_ozTokens_twoUsers_different_mint() internal {
+    function test_two_ozTokens_twoUsers_different_mint() public {
+        //Pre-conditions
+        (ozIToken ozERC20_1, ozIToken ozERC20_2, uint amountInFirst, uint amountInSecond) =
+             test_createAndMint_two_ozTokens_oneUser();
 
+        _mintOzTokens(ozERC20_2, bob, secondTestToken, amountInSecond);
 
+        uint secs = 15;
+        vm.warp(block.timestamp + secs);
+
+        _mock_rETH_ETH();
+
+        //Actions
+        vm.prank(alice);
+        uint rewardsAlice = OZ.claimReward();
+
+        vm.prank(bob);
+        uint rewardsBob = OZ.claimReward();
+
+        //Post-condtions
+        uint rewardRate = _getRewardRate();
+
+        uint ozlBalanceAlice_1 = ozERC20_1.balanceOf(alice);
+        // uint ozlBalanceBob_1 = ozERC20_1.balanceOf(bob);
+        uint ozlBalanceAlice_2 = ozERC20_2.balanceOf(alice);
+        uint ozlBalanceBob_2 = ozERC20_2.balanceOf(bob);
+
+        assertTrue(ozlBalanceAlice_2 == ozlBalanceBob_2);
+        assertTrue(ozlBalanceAlice_2 > 0);
+
+        assertTrue((rewardsBob + rewardsAlice) / 1000 == (rewardRate * secs) / 1000);
+        assertTrue(rewardsBob < rewardsAlice);
 
 
     }
