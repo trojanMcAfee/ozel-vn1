fb4523840e6e7759cadc5a20a39269b27e7e2bac
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-01-28 23:47:54 +0100

    Refactored

--- test/foundry/MultipleOzTokens.t.sol
+++ test/foundry/MultipleOzTokens.t.sol
@@ -13,69 +13,6 @@ import "forge-std/console.sol";
 contract MultipleOzTokensTest is TestMethods {
 
 
-    //Tests OZL rewards accrual between three different users on three ozTokens
-    function test_three_ozTokens_threeUsers_four_mints() public {
-        //Pre-conditions
-        (ozIToken ozERC20_1, ozIToken ozERC20_2,, uint amountInSecond, uint amountInThird) =
-             test_createAndMint_two_ozTokens_oneUser();
-
-        ozIToken ozERC20_3 = ozIToken(OZ.createOzToken(
-            thirdTestToken, "Ozel-ERC20-3", "ozERC20_3"
-        ));
-
-        _mintOzTokens(ozERC20_2, bob, secondTestToken, amountInSecond);
-        _mintOzTokens(ozERC20_3, charlie, thirdTestToken, amountInThird);
-
-        uint secs = 15;
-        vm.warp(block.timestamp + secs);
-
-        _mock_rETH_ETH();
-
-        uint balAlice_1 = ozERC20_1.balanceOf(alice);
-        uint balAlice_2 = ozERC20_2.balanceOf(alice);
-
-        uint balBob_2 = ozERC20_2.balanceOf(bob);
-        uint balCharlie_3 = ozERC20_3.balanceOf(charlie);
-
-        (
-            , uint circulatingSupplyPre,
-            uint pendingAllocationPre,
-            uint recicledSupplyPre,
-        ) = OZ.getRewardsData();
-
-        assertTrue(circulatingSupplyPre == 0);
-        assertTrue(pendingAllocationPre == communityAmount);
-        assertTrue(recicledSupplyPre == 0);
-
-        //Actions
-        vm.prank(alice);
-        uint claimedAlice = OZ.claimReward();
-
-        vm.prank(bob);
-        uint claimedBob = OZ.claimReward();
-
-        vm.prank(charlie);
-        uint claimedCharlie = OZ.claimReward();
-
-        //Post-conditions
-        assertTrue((balBob_2 * 3) / 1e17 == (balAlice_1 + balAlice_2) / 1e17);
-        assertTrue(((balBob_2 / 3) + balCharlie_3) / 1e18 == balBob_2 / 1e18);
-
-        assertTrue((claimedBob * 3) / 1e15 == claimedAlice / 1e15);
-        assertTrue(((claimedBob / 3) + claimedCharlie) / 1e16 == claimedBob / 1e16);
-
-        (
-            , uint circulatingSupplyPost,
-            uint pendingAllocationPost,
-            uint recicledSupplyPost,
-        ) = OZ.getRewardsData();
-
-        assertTrue(circulatingSupplyPost / 1e4 == (claimedAlice + claimedBob + claimedCharlie) / 1e4);
-        assertTrue(pendingAllocationPost + circulatingSupplyPost == communityAmount);
-        assertTrue(recicledSupplyPre == 0);
-    }
-
-
     //Tests the creation of different ozTokens and that their minting of tokens is 
     //done properly. 
     function test_createAndMint_two_ozTokens_oneUser() public returns(
@@ -204,6 +141,66 @@ contract MultipleOzTokensTest is TestMethods {
     }
 
 
+    //Tests OZL rewards accrual between three different users on three ozTokens
+    function test_three_ozTokens_threeUsers_four_mints() public {
+        //Pre-conditions
+        (ozIToken ozERC20_1, ozIToken ozERC20_2,, uint amountInSecond, uint amountInThird) =
+             test_createAndMint_two_ozTokens_oneUser();
 
+        ozIToken ozERC20_3 = ozIToken(OZ.createOzToken(
+            thirdTestToken, "Ozel-ERC20-3", "ozERC20_3"
+        ));
+
+        _mintOzTokens(ozERC20_2, bob, secondTestToken, amountInSecond);
+        _mintOzTokens(ozERC20_3, charlie, thirdTestToken, amountInThird);
+
+        uint secs = 15;
+        vm.warp(block.timestamp + secs);
+
+        _mock_rETH_ETH();
+
+        uint balAlice_1 = ozERC20_1.balanceOf(alice);
+        uint balAlice_2 = ozERC20_2.balanceOf(alice);
+
+        uint balBob_2 = ozERC20_2.balanceOf(bob);
+        uint balCharlie_3 = ozERC20_3.balanceOf(charlie);
+
+        (
+            , uint circulatingSupplyPre,
+            uint pendingAllocationPre,
+            uint recicledSupplyPre,
+        ) = OZ.getRewardsData();
+
+        assertTrue(circulatingSupplyPre == 0);
+        assertTrue(pendingAllocationPre == communityAmount);
+        assertTrue(recicledSupplyPre == 0);
+
+        //Actions
+        vm.prank(alice);
+        uint claimedAlice = OZ.claimReward();
+
+        vm.prank(bob);
+        uint claimedBob = OZ.claimReward();
+
+        vm.prank(charlie);
+        uint claimedCharlie = OZ.claimReward();
+
+        //Post-conditions
+        assertTrue((balBob_2 * 3) / 1e17 == (balAlice_1 + balAlice_2) / 1e17);
+        assertTrue(((balBob_2 / 3) + balCharlie_3) / 1e18 == balBob_2 / 1e18);
+
+        assertTrue((claimedBob * 3) / 1e15 == claimedAlice / 1e15);
+        assertTrue(((claimedBob / 3) + claimedCharlie) / 1e16 == claimedBob / 1e16);
+
+        (
+            , uint circulatingSupplyPost,
+            uint pendingAllocationPost,
+            uint recicledSupplyPost,
+        ) = OZ.getRewardsData();
+
+        assertTrue(circulatingSupplyPost / 1e4 == (claimedAlice + claimedBob + claimedCharlie) / 1e4);
+        assertTrue(pendingAllocationPost + circulatingSupplyPost == communityAmount);
+        assertTrue(recicledSupplyPost == 0);
+    }
 
 }
