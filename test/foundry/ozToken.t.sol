10bb1de0e31e9befa3bc10676034bb6e5d996b7e
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-01-29 15:00:24 +0100

    Done with test_catch_internal_errors_mint()

--- test/foundry/ozToken.t.sol
+++ test/foundry/ozToken.t.sol
@@ -5,13 +5,19 @@ pragma solidity 0.8.21;
 import {TestMethods} from "./TestMethods.sol";
 import {ozIToken} from "../../contracts/interfaces/ozIToken.sol";
 import {IERC20Permit} from "../../contracts/interfaces/IERC20Permit.sol";
-import "../../contracts/Errors.sol";
 import {HelpersLib} from "./HelpersLib.sol";
+import {Type} from "./AppStorageTests.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AmountsIn} from "../../contracts/AppStorage.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../../contracts/Errors.sol";
 
 import "forge-std/console.sol";
 
 
 contract ozTokenTest is TestMethods {
+
+    using SafeERC20 for IERC20;
 
     function test_catch_internal_errors_mint() public {
         //Pre-conditions  
@@ -22,23 +28,23 @@ contract ozTokenTest is TestMethods {
         (uint rawAmount,,) = _dealUnderlying(Quantity.SMALL, true);
         uint amountIn = rawAmount * 10 ** IERC20Permit(testToken).decimals();
 
-        vm.expectRevert(
-            abi.encodeWithSelector(OZError22.selector, 'SafeERC20: low-level call failed')
-        );
+        // vm.expectRevert(
+        //     abi.encodeWithSelector(OZError22.selector, 'SafeERC20: low-level call failed')
+        // );
 
         // _mintOzTokens(ozERC20_1, alice, testToken, amountIn);
 
         (bytes memory data) = _createDataOffchain(
-            ozERC20_1, amountIn_, ALICE_PK, alice, testToken, Type.IN
+            ozERC20_1, amountIn, ALICE_PK, alice, testToken, Type.IN
         );
 
         (uint[] memory minAmountsOut,,,) = HelpersLib.extract(data);
 
-        vm.startPrank(user_);
-        IERC20(token_).safeApprove(address(OZ), amountIn_);
+        vm.startPrank(alice);
+        IERC20(testToken).safeApprove(address(OZ), amountIn);
 
         AmountsIn memory amounts = AmountsIn(
-            amountIn_,
+            amountIn,
             minAmountsOut
         );
 
@@ -46,7 +52,7 @@ contract ozTokenTest is TestMethods {
             abi.encodeWithSelector(OZError22.selector, 'SafeERC20: low-level call failed')
         );
 
-        ozERC20_.mint(abi.encode(amounts, user_));         
+        ozERC20_1.mint(abi.encode(amounts, alice));         
         vm.stopPrank();
 
     }
