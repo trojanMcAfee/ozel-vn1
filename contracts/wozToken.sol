1a6e5297d4ffe8910fb01af0e6aca06c1a475cbf
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-02-13 00:06:01 +0100

    All tests passing

--- contracts/wozToken.sol
+++ contracts/wozToken.sol
@@ -105,18 +105,4 @@ contract wozToken is ERC20PermitUpgradeable {
     }
 
 
-    //--------------
-
-    // function DOMAIN_SEPARATOR() external view returns (bytes32) {}
-    // function nonces(address owner) external view returns (uint256) {}
-    // function permit(
-    //     address owner,
-    //     address spender,
-    //     uint256 value,
-    //     uint256 deadline,
-    //     uint8 v,
-    //     bytes32 r,
-    //     bytes32 s
-    // ) external {}
-
 }
