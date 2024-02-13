600237ff67f97fb042a1d71cd25fa44044ff3f28
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-02-13 22:46:37 +0100

    Done with test_pause_whole_system()

--- test/foundry/Pause.t.sol
+++ test/foundry/Pause.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.21;
 
 
 import {TestMethods} from "./TestMethods.sol";
+import "../../contracts/Errors.sol";
 
 
 import "forge-std/console.sol";
@@ -12,9 +13,24 @@ import "forge-std/console.sol";
 contract PauseTest is TestMethods {
 
     function test_pause_whole_system() public {
+        //Pre-condition
         uint ethPrice = OZ.ETH_USD();
         assertTrue(ethPrice > 0);
 
+        //Action
+        uint sectionToPause = 1;
+        vm.prank(owner);
+        OZ.pause(sectionToPause, true);
+
+        //Post-condition
+        vm.expectRevert(
+            abi.encodeWithSelector(OZError27.selector, sectionToPause)
+        );
+        OZ.ETH_USD();
+    }
+
+    function test_pause_ozTokens() public {
+        
     }
 
 }
