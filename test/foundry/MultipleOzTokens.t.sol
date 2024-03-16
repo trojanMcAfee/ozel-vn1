4c2900dadd61f8eed2ef2ee2136ee62e5f4e4afa
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-03-16 18:03:09 +0100

    Refactored

--- test/foundry/MultipleOzTokens.t.sol
+++ test/foundry/MultipleOzTokens.t.sol
@@ -173,31 +173,23 @@ contract MultipleOzTokensTest is TestMethods {
         uint claimedCharlie = OZ.claimReward();
 
         //Post-conditions
-
-        console.log(11);
         assertTrue((balBob_2 * 3) / 1e17 == (balAlice_1 + balAlice_2) / 1e17);
-
-        console.log('balAlice_1: ', balAlice_1);
-        console.log('balAlice_2: ', balAlice_2);
-
-        console.log('');
-        console.log('balBob_2: ', balBob_2);
-        console.log('balCharlie_3: ', balCharlie_3); 
-
         uint deltaMultipleOz = stdMath.abs(int((balBob_2 / 3) + balCharlie_3) - int(balBob_2));
         
-        console.log('delta: ', _fm(deltaMultipleOz, 8));
+        /**
+         * This check proves that the difference between ozToken balances between multiple ozTokens,
+         * users, and minting quantities is 3334000033, which formatted to ^10 is 0.33 ozTokens. 
+         *
+         * This number (0.33) is constant regardless of whether the mining amount is 50 units of 500k.
+         * So for a 50 USDC mint, it represents a 0.66% and for 500k mint, it's a 0.000066% difference.
+         */
         assertTrue(_fm(deltaMultipleOz, 8) == 3334000033);
 
-        // assertTrue(((balBob_2 / 3) + balCharlie_3) / 1e18 == balBob_2 / 1e18);
-
-        console.log(3);
-
-        assertTrue((claimedBob * 3) / 1e15 == claimedAlice / 1e15);
-        assertTrue(((claimedBob / 3) + claimedCharlie) / 1e16 == claimedBob / 1e16);
+        assertTrue(_fm(claimedBob * 3, 15) == _fm(claimedAlice, 15));
+        assertTrue(_fm((claimedBob / 3) + claimedCharlie, 16) == _fm(claimedBob, 16));
 
         (
-            , uint circulatingSupplyPost,
+            ,uint circulatingSupplyPost,
             uint pendingAllocationPost,
             uint recicledSupplyPost,
         ) = OZ.getRewardsData();
