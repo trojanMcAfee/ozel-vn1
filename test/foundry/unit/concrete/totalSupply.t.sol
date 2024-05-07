caf620d0ace85d0b614d44a08467d095b930a010
trojanMcAfee <cdgmachado0@gmail.com>
2024-05-07 22:44:07 +0200

    Modified totalSupply.tree

--- test/foundry/unit/concrete/totalSupply.t.sol
+++ test/foundry/unit/concrete/totalSupply.t.sol
@@ -1,12 +1,28 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.21;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.0;
 
 contract totalSupply_Unit_Concrete_Test {
-    function test_GivenTotalSharesEqual0() external {
+    modifier whenTheUnderlyingHas6Decimals() {
+        _;
+    }
+
+    function test_GivenTotalSharesEqual0_6() external whenTheUnderlyingHas6Decimals {
         // it should return 0.
     }
 
-    function test_GivenTotalSharesIsNotEqualTo0() external {
+    function test_GivenTotalSharesIsNotEqualTo0_6() external whenTheUnderlyingHas6Decimals {
+        // it should return the sum of all users' balances.
+    }
+
+    modifier whenTheUnderlyingHas18Decimals() {
+        _;
+    }
+
+    function test_GivenTotalSharesEqual0() external whenTheUnderlyingHas18Decimals {
+        // it should return 0.
+    }
+
+    function test_GivenTotalSharesIsNotEqualTo0() external whenTheUnderlyingHas18Decimals {
         // it should return the sum of all users' balances.
     }
 }
