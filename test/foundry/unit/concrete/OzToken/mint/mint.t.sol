d8ac90c41685da3767234febba4856a9df3dd736
dnyrm <cdgmachado0@gmail.com>
2024-05-19 21:54:57 +0200

    Added new condition to Mint_Unit_Concrete_test

--- test/foundry/unit/concrete/OzToken/mint/mint.t.sol
+++ test/foundry/unit/concrete/OzToken/mint/mint.t.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.24;
 
+
 import {Mint_Core} from "./Mint_Core.sol";
 
 
 contract Mint_Unit_Concrete_test is Mint_Core {
+
     function test_WhenOwnerIsZero() external {
         it_should_revert(6, Revert.OWNER);
         it_should_revert(18, Revert.OWNER);
@@ -14,37 +16,10 @@ contract Mint_Unit_Concrete_test is Mint_Core {
         _;
     }
 
-    modifier whenAnalysingMinAmountsOut() {
-        _;
-    }
-
-    // function test_WhenAtLeastOneElementFromMinAmountsOutIsZero()
-    //     external
-    //     whenAnalysingMintData
-    //     whenAnalysingMinAmountsOut
-    // {
-    //     it_should_mint(6);
-    //     // it should mint with slippage_18dec
-    // }
-
-    // function test_WhenAtLeastOneElementMinAmountsOutIsUintMax()
-    //     external
-    //     whenAnalysingMintData
-    //     whenAnalysingMinAmountsOut
-    // {
-    //     // it should throw error_6dec
-    //     // it should throw error_18dec
-    // }
-
     function test_WhenAmountInIsZero() external whenAnalysingMintData {
         it_should_revert(6, Revert.AMOUNT_IN);
         it_should_revert(18, Revert.AMOUNT_IN);
     }
-
-    // function test_WhenSlippageIsNotEnough() external whenAnalysingMintData {
-    //     // it should throw error_6dec
-    //     // it should throw error_18dec
-    // }
 
     function test_WhenReceiverIsZero() external whenAnalysingMintData {
         it_should_revert(6, Revert.RECEIVER);
@@ -64,5 +39,9 @@ contract Mint_Unit_Concrete_test is Mint_Core {
     function test_WhenUserDoesntHaveUnderlyingBalance() external {
         it_should_throw_error_22(6);
         it_should_throw_error_22(18);
+    }
+
+    function test_WhenYouTryToReenter() external {
+        // it reverts
     }
 }
