8fd5252d3e135c58bd17e7cd7938982fbd2696f3
dnyrm <cdgmachado0@gmail.com>
2024-05-20 10:38:23 +0200

    Done with TotalSupply_Unit_Concrete_Test

--- test/foundry/unit/concrete/OzToken/totalSupply/totalSupply.t.sol
+++ test/foundry/unit/concrete/OzToken/totalSupply/totalSupply.t.sol
@@ -1,25 +1,18 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.24;
-
 
 import {TotalSupply_Core} from "./totalSupply_core.sol";
 
 
 contract TotalSupply_Unit_Concrete_Test is TotalSupply_Core {
 
-    function test_GivenTotalSharesEqual0_6() external whenTheUnderlyingHas6Decimals {
+    function test_WhenTotalSharesIs0() external {
         it_should_return_0(6);
-    }
-
-    function test_GivenTotalSharesIsNotEqualTo0_6() external whenTheUnderlyingHas6Decimals {
-        it_should_return_the_sum_of_all_users_balances(6);
-    }
-
-    function test_GivenTotalSharesEqual0_18() external whenTheUnderlyingHas18Decimals {
         it_should_return_0(18);
     }
 
-    function test_GivenTotalSharesIsNotEqualTo0_18() external whenTheUnderlyingHas18Decimals {
+    function test_WhenTotalSharesIsNot0() external {
+        it_should_return_the_sum_of_all_users_balances(6);
         it_should_return_the_sum_of_all_users_balances(18);
     }
 }
--- test/foundry/unit/concrete/OzToken/totalSupply/totalSupply_core.sol
+++ test/foundry/unit/concrete/OzToken/totalSupply/totalSupply_core.sol
@@ -5,13 +5,15 @@ pragma solidity 0.8.24;
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {Mock} from "../../../../base/AppStorageTests.sol";
 import {SharedConditions} from "../SharedConditions.sol";
+import {ozIToken} from "./../../../../../../contracts/interfaces/ozIToken.sol";
 
 
 contract TotalSupply_Core is SharedConditions {
 
     function it_should_return_0(uint decimals_) internal skipOrNot {
         //Pre-conditions
-        assertEq(IERC20(testToken_internal).decimals(), decimals_);
+        (ozIToken ozERC20, address underlying) = setUpOzToken(decimals_);
+        assertEq(IERC20(underlying).decimals(), decimals_);
         assertEq(ozERC20.totalShares(), 0);
 
         //Post-condition
@@ -20,16 +22,17 @@ contract TotalSupply_Core is SharedConditions {
 
     function it_should_return_the_sum_of_all_users_balances(uint decimals_) internal skipOrNot {
         //Pre-conditions
-        assertEq(IERC20(testToken_internal).decimals(), decimals_);
+        (ozIToken ozERC20, address underlying) = setUpOzToken(decimals_);
+        assertEq(IERC20(underlying).decimals(), decimals_);
         
         uint rawAmount = 100;
         _mock_rETH_ETH_unit(Mock.PREACCRUAL_UNI_NO_DEVIATION);
 
-        uint amountIn = (rawAmount / 3) * 10 ** IERC20(testToken_internal).decimals();
+        uint amountIn = (rawAmount / 3) * 10 ** IERC20(underlying).decimals();
 
         //Actions
-        _mintOzTokens(ozERC20, alice, testToken_internal, amountIn);
-        _mintOzTokens(ozERC20, bob, testToken_internal, amountIn);
+        _mintOzTokens(ozERC20, alice, underlying, amountIn);
+        _mintOzTokens(ozERC20, bob, underlying, amountIn);
 
         //Post-condition
         assertEq(
