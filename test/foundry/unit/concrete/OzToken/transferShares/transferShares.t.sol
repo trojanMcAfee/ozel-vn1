6b27b433ec5cc196ebd73839882780c6a59c1103
dnyrm <cdgmachado0@gmail.com>
2024-05-23 22:05:42 +0200

    Done with TransferShares_Unit_Concrete_test.sol

--- test/foundry/unit/concrete/OzToken/transferShares/transferShares.t.sol
+++ test/foundry/unit/concrete/OzToken/transferShares/transferShares.t.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.24;
 
-
 import {TransferShares_Core} from "./TransferShares_Core.sol";
-
 
 contract TransferShares_Unit_Concrete_test is TransferShares_Core {
     function test_WhenSenderHasTheNecessaryShares() external {
@@ -11,17 +9,17 @@ contract TransferShares_Unit_Concrete_test is TransferShares_Core {
         it_should_transfer_shares(18);
     }
 
-    function test_WhenSenderDoesntHaveTheNecessaryShares() external {
+    function test_RevertOn_WhenSenderDoesntHaveTheNecessaryShares() external {
         it_should_throw_error_07(6);
         it_should_throw_error_07(18);
     }
 
-    function test_WhenRecipientIsZero() external {
+    function test_RevertOn_WhenRecipientIsZero() external {
         it_should_throw_error_04(6);
         it_should_throw_error_04(18);
     }
 
-    function test_WhenRecipientIsSelf() external {
+    function test_RevertOn_WhenRecipientIsSelf() external {
         it_should_throw_error_42(6);
         it_should_throw_error_42(18);
     }
