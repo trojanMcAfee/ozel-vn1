7214b014ebd4268530bf1730c8e4e6c84c5aeaa6
dnyrm <cdgmachado0@gmail.com>
2024-06-28 19:24:26 +0200

    Create HelpersLogic.sol

---
+++ contracts/Accumulator.sol
@@ -0,0 +1,25 @@
+// // SPDX-License-Identifier: GPL-2.0-or-later
+// pragma solidity 0.8.24;
+
+
+// import {FenwickTree} from "./FenwickTree.sol";
+
+
+// contract Accumulator is FenwickTree  {
+
+//     uint accumulated;
+//     uint maxNumber;
+
+
+//     constructor(uint size_, uint maxNumber_) FenwickTree(size_) {
+//         maxNumber = maxNumber_;
+//     }
+
+
+//     function getAccumulated() external view returns(uint) {
+//         query(index);
+//     }
+
+
+
+// }
---
+++ test/foundry/base/CoreTokenLogic/helpers/HelpersLogic.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.24;
+
+import {FixedPointMathLib} from "./../../../../../contracts/libraries/FixedPointMathLib.sol";
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import {IVault, IPool, IAsset} from "./../../../../../contracts/interfaces/IBalancer.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {TestMethods} from "../../../../foundry/base/TestMethods.sol";
+import {ozIToken} from "./../../../../../contracts/interfaces/ozIToken.sol";
+import {AmountsIn} from "./../../../../../contracts/AppStorage.sol";
+
+import "forge-std/console.sol";
+
+
+contract HelpersLogic is TestMethods {
+
+
+    using FixedPointMathLib for uint;
+
+
+    function _constructUniSwap(uint amountIn_) internal view returns(ISwapRouter.ExactInputSingleParams memory) {
+        return ISwapRouter.ExactInputSingleParams({ 
+                tokenIn: wethAddr,
+                tokenOut: testToken, 
+                fee: uniPoolFee, 
+                recipient: address(OZ),
+                deadline: block.timestamp,
+                amountIn: amountIn_,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+    }
+
+
+    function _constructBalancerSwap(bool isRebase_) internal view returns(
+        IVault.SingleSwap memory, 
+        IVault.FundManagement memory
+    ) {
+        IAsset tokenIn = IAsset(isRebase_ ? rEthAddr : wethAddr);
+        IAsset tokenOut = IAsset(isRebase_ ? wethAddr : rEthAddr);
+        uint amountIn = isRebase_ ? 1924728482031253 : 28398352812392632;
+
+        IVault.SingleSwap memory singleSwap = IVault.SingleSwap({
+            poolId: IPool(rEthWethPoolBalancer).getPoolId(),
+            kind: IVault.SwapKind.GIVEN_IN,
+            assetIn: tokenIn,
+            assetOut: tokenOut,
+            amount: amountIn,
+            userData: new bytes(0)
+        });
+
+        IVault.FundManagement memory funds = IVault.FundManagement({
+            sender: address(OZ),
+            fromInternalBalance: false, 
+            recipient: payable(address(OZ)),
+            toInternalBalance: false
+        });
+
+        return (singleSwap, funds);
+    }
+
+    function _balancerPart(uint blockAccrual, bool isRebase_) internal returns(uint) {
+        (
+            IVault.SingleSwap memory singleSwap, 
+            IVault.FundManagement memory funds
+        ) = _constructBalancerSwap(isRebase_);
+
+        uint rateRETHETH = isRebase_ ? 1154401364401861932 : 1111038024285138135;
+        uint amountToSwap = isRebase_ ? 1924728482031253 : 28398352812392632;
+        uint swappedAmount = isRebase_ ? 
+            rateRETHETH.mulDivDown(amountToSwap, 1 ether) :
+            amountToSwap.mulDivDown(1 ether, rateRETHETH);
+
+        // console.log('amountToSwap: ', amountToSwap);
+
+        uint oldAmountWETH = IERC20(wethAddr).balanceOf(address(this));
+        // address tokenToSend = isRebase_ ? wethAddr : rEthAddr;
+        console.log('');
+
+        // console.log('');
+        // console.log('--- in balancerPart ---');
+        // console.log('amountIn: ', singleSwap.amount);
+        // console.log('assetIn: ', address((singleSwap.assetIn)));
+        // console.log('assetOut: ', address(singleSwap.assetOut));
+        // console.log('amountOut: ', swappedAmount);
+        // console.log('');
+        
+        vm.mockCall(
+            vaultBalancer,
+            abi.encodeWithSelector(IVault.swap.selector, singleSwap, funds, 0, blockAccrual),
+            abi.encode(swappedAmount)
+        );
+        deal(isRebase_ ? wethAddr : rEthAddr, address(OZ), swappedAmount);
+        // assertTrue(oldAmountWETH < IERC20(wethAddr).balanceOf(address(OZ)));
+
+        return amountToSwap;
+    }
+
+      //---- mock UNISWAP WETH<>USDC swap (not need for now since ETHUSD hasn't chan ged)----
+        // ISwapRouter.ExactInputSingleParams memory params = _constructUniSwap(swappedAmountWETH);
+
+        // vm.mockCall(
+        //     swapRouterUni,
+        //     abi.encodeWithSelector(ISwapRouter.exactInputSingle.selector, params),
+        //     abi.encode()
+        // );
+
+    function _bobDeposit(ozIToken ozERC20, uint amountIn) internal {
+        bytes memory mintData = OZ.getMintData(amountIn, OZ.getDefaultSlippage(), bob, address(ozERC20));
+        (AmountsIn memory amts,) = abi.decode(mintData, (AmountsIn, address));
+
+        payable(bob).transfer(500 ether);
+
+        vm.startPrank(bob);
+
+        IERC20(testToken).approve(address(OZ), amountIn);
+        ozERC20.mint2{value: amts.amountInETH}(mintData, bob, true);
+
+        vm.stopPrank();
+    }
+
+}
